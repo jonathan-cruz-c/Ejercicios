d33ef8b9cadfa4e04000649d49acf5fb80aae5a0
Levy <levycrstc@gmail.com>
2025-02-11 16:25:22 -0600

    Yair Levy Cruz Rios

---
+++ YairLevyCruzRios/Suma.java
@@ -0,0 +1,9 @@
+class Suma{
+    public static void main (String [] args){
+    int suma(int a, int b){
+        System.out.println("Suma de dos numeros");
+        return a+b;
+    }
+        System.out.println(suma(10,20));
+    }   
+}
